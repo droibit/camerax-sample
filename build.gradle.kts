65ed4b9a1cf5bf20eefd13f10d63dc9d0a5950cf
Shinya Kumagai <roomful.rooms@gmail.com>
2020-07-22 00:10:00 +0900

    Update to AGP v4.0.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ buildscript {
     jcenter()
   }
   dependencies {
-    classpath("com.android.tools.build:gradle:4.0.0")
+    classpath("com.android.tools.build:gradle:4.0.1")
     classpath(kotlin("gradle-plugin", version = kotlinVersion))
     classpath("com.google.dagger:hilt-android-gradle-plugin:$daggerVersion")
     classpath("androidx.navigation:navigation-safe-args-gradle-plugin:$navigationVersion")
