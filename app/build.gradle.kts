29a7738a7c21df35e8a6d834cb32155da7d359a4
Shinya Kumagai <roomful.rooms@gmail.com>
2021-01-08 11:08:02 +0900

    Update some dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,18 +45,18 @@ dependencies {
   implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:$coroutinesVersion")
 
   implementation("androidx.core:core-ktx:1.3.2")
-  implementation("androidx.fragment:fragment-ktx:1.3.0-beta02")
+  implementation("androidx.fragment:fragment-ktx:1.3.0-rc01")
   implementation("androidx.appcompat:appcompat:1.2.0")
   implementation("androidx.constraintlayout:constraintlayout:2.0.4")
   implementation("androidx.viewpager2:viewpager2:1.0.0")
 
-  val cameraxVersion = "1.0.0-beta12"
+  val cameraxVersion = "1.0.0-rc01"
   implementation("androidx.camera:camera-core:$cameraxVersion")
   implementation("androidx.camera:camera-camera2:$cameraxVersion")
   implementation("androidx.camera:camera-lifecycle:$cameraxVersion")
-  implementation("androidx.camera:camera-view:1.0.0-alpha19")
+  implementation("androidx.camera:camera-view:1.0.0-alpha20")
 
-  val lifecycleVersion = "2.3.0-beta01"
+  val lifecycleVersion = "2.3.0-rc01"
   implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycleVersion")
   implementation("androidx.lifecycle:lifecycle-livedata-ktx:$lifecycleVersion")
 
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 buildscript {
-  val kotlinVersion by extra("1.4.20")
+  val kotlinVersion by extra("1.4.21")
   val daggerVersion by extra("2.30.1-alpha")
   val navigationVersion by extra("2.3.2")
 
